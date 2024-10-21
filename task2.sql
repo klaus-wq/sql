c9b5c344dee77f5046acf19f1581aa119e261041
klaus <70817966+klaus-wq@users.noreply.github.com>
2024-10-22 01:54:02 +0700

    add task 3

--- task2.sql
+++ task2.sql
@@ -1,6 +1,6 @@
 1. SELECT ProductName, UnitsInStock FROM Products;
 2. SELECT ProductName, UnitPrice FROM Products WHERE UnitPrice < 20;
-3. SELECT * FROM Orders WHERE (Freight > 11.7) AND (Freight < 98.1);
+3. SELECT * FROM Orders WHERE (Freight >= 11.7) AND (Freight <= 98.1);
 4. SELECT * FROM Employees WHERE (TitleOfCourtesy = 'Mr.') OR (TitleOfCourtesy = 'Dr.');
 5. SELECT * FROM Suppliers WHERE Country = 'Japan';
 6. SELECT * FROM Orders WHERE (EmployeeID = 2) OR (EmployeeID = 4) OR (EmployeeID = 8);
---
+++ task3.sql
@@ -0,0 +1,3 @@
+1. SELECT * FROM Customers WHERE ContactName LIKE 'C%';
+2. SELECT * FROM Orders WHERE Freight BETWEEN 100 AND 200 AND (ShipCountry in ('USA', 'France'));
+3. SELECT DISTINCT * FROM EmployeeTerritories WHERE TerritoryID BETWEEN 6897 AND 31000;
