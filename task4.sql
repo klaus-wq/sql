08533d6a3e2442e2a4717c9640f22ed32371e3b1
klaus <70817966+klaus-wq@users.noreply.github.com>
2024-10-23 23:37:19 +0700

    add task 4

---
+++ task4.sql
@@ -0,0 +1,4 @@
+1. SELECT *  FROM Employees ORDER BY BirthDate DESC, Country;
+2. SELECT *  FROM Employees WHERE Region IS NOT NULL ORDER BY BirthDate DESC, Country;
+3. SELECT AVG(UnitPrice), MIN(UnitPrice), MAX(UnitPrice) FROM [Order Details];
+4. SELECT COUNT(DISTINCT City) FROM Customers;
