6fe2cc5e8229c3fb9954d945653e9c61017cf501
klaus <70817966+klaus-wq@users.noreply.github.com>
2024-10-28 01:19:47 +0700

    add task 6

--- task5.sql
+++ task5.sql
@@ -1,2 +1,2 @@
 1. SELECT ContactType, COUNT(ContactType) FROM Contacts GROUP BY ContactType;
-2. SELECT CategoryID, AVG(UnitPrice) AS AVG_UNIT_PRICE FROM Products GROUP BY CategoryID ORDER BY AVG_UNIT_PRICE;
+2. SELECT CategoryID, AVG(UnitPrice) AS avg_price FROM Products GROUP BY CategoryID ORDER BY avg_price;
