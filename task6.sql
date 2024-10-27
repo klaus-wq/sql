6c570de8687c5568044191dc39a0aa619131c89d
klaus <70817966+klaus-wq@users.noreply.github.com>
2024-10-28 01:20:03 +0700

    add task 6

--- task6.sql
+++ task6.sql
@@ -1,3 +1,3 @@
 1. SELECT Discount * 100 FROM [Order Details];
 2. SELECT * FROM [Order Details] WHERE ProductID IN (SELECT ProductId FROM Products WHERE UnitsInStock > 40);
-3. SELECT * FROM [Order Details] WHERE ProductID IN   (SELECT ProductId FROM Products WHERE UnitsInStock > 40) AND OrderID IN ( SELECT OrderID FROM Orders WHERE Freight >= 50);
+3. SELECT * FROM [Order Details] WHERE ProductID IN (SELECT ProductId FROM Products WHERE UnitsInStock > 40) AND OrderID IN ( SELECT OrderID FROM Orders WHERE Freight >= 50);
