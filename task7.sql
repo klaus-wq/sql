4042e7e442391069249fed4063e83ccbd0104e62
klaus <70817966+klaus-wq@users.noreply.github.com>
2024-10-30 00:28:28 +0700

    add task 7

---
+++ task7.sql
@@ -0,0 +1,3 @@
+1. SELECT Products.ProductName, Categories.CategoryName FROM Products, Categories WHERE Products.CategoryID = Categories.CategoryID;
+2. SELECT Products.UnitPrice, Products.ProductName FROM Products, [Order Details] WHERE Products.ProductID = [Order Details].ProductID AND [Order Details].UnitPrice < 20;
+3. SELECT Products.UnitPrice, Products.ProductName, Categories.CategoryName FROM Products, [Order Details], Categories WHERE Products.ProductID = [Order Details].ProductID AND Products.CategoryID = Categories.CategoryID AND [Order Details].UnitPrice < 20;
