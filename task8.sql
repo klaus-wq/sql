f8d6378fc946fe39e9c60fd40633e31398f78903
klaus <70817966+klaus-wq@users.noreply.github.com>
2024-11-01 15:38:38 +0700

    add task 8

--- task8.sql
+++ task8.sql
@@ -1,3 +1,6 @@
 1. SELECT t1.CustomerID, t2.CustomerID FROM Customers t1, Customers t2 WHERE t1.CustomerID <> t2.CustomerID AND t1.Region IS NULL AND t2.Region IS NULL;
 2. SELECT OrderID FROM Orders WHERE CustomerID = ANY (SELECT CustomerID FROM Customers WHERE Region IS NOT NULL);
 3. SELECT OrderID FROM Orders WHERE Freight > ALL (SELECT UnitPrice FROM Products);
+
+1-не учла то, что заказчики должны быть уникальными
+2, 3 - нужно было выбрать не всю таблицу, а только id
