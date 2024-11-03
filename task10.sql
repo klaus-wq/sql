28317957af99f24b5d2086f3c62f59d08e3a4142
klaus <70817966+klaus-wq@users.noreply.github.com>
2024-11-03 23:04:01 +0700

    add task 10

---
+++ task10.sql
@@ -0,0 +1,9 @@
+1. SELECT * FROM Customers
+LEFT JOIN Orders
+ON Orders.CustomerID = Customers.CustomerID
+WHERE Orders.CustomerID is NULL;
+2. SELECT 'Customer' As Type, ContactName, City, Country
+FROM Customers
+UNION
+SELECT 'Supplier' As Type, ContactName, City, Country
+FROM Suppliers;
