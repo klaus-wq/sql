fef1cc48e478644ef66cb302f35fb7dd15d7885f
klaus <70817966+klaus-wq@users.noreply.github.com>
2024-11-06 01:58:05 +0700

    add task 12

--- task12.sql
+++ task12.sql
@@ -3,10 +3,12 @@ SET Discount = 0.2
 WHERE Quantity > 50;
 2. UPDATE Contacts
 SET City = 'Piter', Country = 'Russia'
-WHERE City = 'Berlin' AND Country='Germany';
+WHERE City = 'Berlin' OR Country='Germany';
 3. INSERT INTO Shippers (CompanyName, Phone)
 VALUES ('Green', '(503) 555-9654');
 DELETE FROM Shippers
 WHERE ShipperID > 3;
 
 Критерий удаления свежих записей - ShipperID > 3.
+
+2-Нужно OR вместо AND. ?
